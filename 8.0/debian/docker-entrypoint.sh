2150a749132c02648282393b73af1340b6dcb7c9
namuk2004 <namuk2004@naver.com>
2026-02-26 11:34:09 +0900

    updated docker-entrypoint.sh
    Signed-off-by: namuk2004 <namuk2004@naver.com>

--- 8.0/debian/docker-entrypoint.sh
+++ 8.0/debian/docker-entrypoint.sh
@@ -9,16 +9,16 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'valkey-server' ]; then
-    if [ "$(id -u)" = '0' ]; then
-        find . \! -user valkey -exec chown valkey '{}' +
-        exec setpriv --reuid=valkey --regid=valkey --clear-groups -- "$0" "$@"
-    else
-        if [ ! -w . ]; then
-            echo >&2 "error: directory '$(pwd)' is not writable by current user ($(id -u))"
-            echo >&2 "  Check mount permissions or run as root to allow chown"
-            exit 1
-        fi
-    fi
+	if [ "$(id -u)" = '0' ]; then
+		find . \! -user valkey -exec chown valkey '{}' +
+		exec setpriv --reuid=valkey --regid=valkey --clear-groups -- "$0" "$@"
+	else
+		if [ ! -w . ]; then
+			echo >&2 "error: directory '$(pwd)' is not writable by current user ($(id -u))"
+			echo >&2 "  Check mount permissions or run as root to allow chown"
+			exit 1
+		fi
+	fi
 fi
 
 # set an appropriate umask (if one isn't set already)
